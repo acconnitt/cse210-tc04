736309ec49feb18a3633e788da467ffb5718dcca
markvagil <83776097+markvagil@users.noreply.github.com>
2021-05-15 10:27:19 -0500

    Update __main__.py

--- hilo/__main__.py
+++ hilo/__main__.py
@@ -2,6 +2,8 @@
 # Test comment from Bro. Lythgoe
 
 from game.dealer import Dealer
-
+#This imports the dealer class
 dealer = Dealer()
+#This initializes the dealer class
 dealer.play_game()
+#This calls the function in the dealer class to start playing the game
